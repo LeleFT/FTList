b49050ef60684e3cf4c6e8c1a2385c158fadb522
LeleFT <leleft.ita@gmail.com>
2022-03-23 16:00:52 +0100

    Fixed some javadoc comments

--- src/main/java/ft/list/event/FTSelectionObservable.java
+++ src/main/java/ft/list/event/FTSelectionObservable.java
@@ -4,7 +4,7 @@ import javax.swing.event.ListSelectionListener;
 
 /**
  * The observable interface that allows an <code>FTList</code> object to register
- * a <code>ListSelectionListener</code>.<br />
+ * a <code>ListSelectionListener</code>.<br>
  * The <code>ListSelectionListener</code> object will be notified for changes made
  * to the selection model of the list.
  * 
